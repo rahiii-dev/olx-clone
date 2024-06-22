8303b618f34a2cd02d05606839c98f5c88309566
rahiii-dev <rahiofcl@gmail.com>
2024-06-22 17:44:01 +0530

    added env configeration

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,14 +3,23 @@ import { getAuth } from 'firebase/auth';
 import { collection, getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+const {
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_AUTH_DOMAIN,
+  VITE_FIREBASE_PROJECT_ID,
+  VITE_FIREBASE_STORAGE_BUCKET,
+  VITE_FIREBASE_MESSAGING_SENDER_ID,
+  VITE_FIREBASE_APP_ID
+} = import.meta.env;
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAaELwC1GetLBZQhYRqlfskqFtI6ttafrM",
-  authDomain: "myapp-acfcd.firebaseapp.com",
-  projectId: "myapp-acfcd",
-  storageBucket: "myapp-acfcd.appspot.com",
-  messagingSenderId: "972790748287",
-  appId: "1:972790748287:web:9be4ef66457895a4449b4a"
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
